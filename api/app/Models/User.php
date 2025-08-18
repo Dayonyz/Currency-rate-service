4e5cdf56ca41535f96e29d6e2e834107637f15f2
Pisklov Denys <denys.pisklov@gmail.com>
2025-08-18 15:53:07 +0300

    Useless lines

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;;
-
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Queue\SerializesAndRestoresModelIdentifiers;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
